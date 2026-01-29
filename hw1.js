9bb1e59dd948b00225f44db5de9945b49559f87e
Shirokome341 <yuchen.jiang103@gmail.com>
2026-01-28 20:49:32 -0500

    String part complement with console output.

--- hw1.js
+++ hw1.js
@@ -1,3 +1,5 @@
+//Part 1: Arrays
+
 //Get the total sum out of single array;
 function sum(arr) {
     let total = 0;
@@ -40,4 +42,37 @@ const myArray = [14, 23, 9, 18, 5]
 console.log("Testing sum: " + sum(myArray) + "\n")
 console.log("Testing average: " + average(myArray).toFixed(2) + "\n")
 console.log("Testing minimum: " + min(myArray) + "\n")
-console.log("Testing maximum: " + max(myArray) + "\n")
+console.log("Testing maximum: " + max(myArray) + "\n\n")
+
+//-------------------------------------------------------------------------------
+//Part 2: Strings
+
+//Capitalize any given string;
+function capitalize(str) {
+    return str.toUpperCase();
+}
+
+//Reverse the character order of given string;
+function reverse(str) {
+    const stringInArray = str.split('');
+    const reverseArray = stringInArray.reverse();
+    return reverseArray.join('');
+}
+
+//Counts how many vowels from given string;
+function countVowels(str) {
+    const vowels = "aeiouAEIOU";
+    let counter = 0;
+
+    for (const letter of str) {
+        if (vowels.includes(letter)) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+const myString = "Hello"
+console.log("Testing capitalize: " + capitalize(myString) + "\n")
+console.log("Testing reverse: " + reverse(myString) + "\n")
+console.log("Testing countVowels: " + countVowels(myString) + "\n")
